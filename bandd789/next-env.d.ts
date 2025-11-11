ff0616759202a52c7aed7a4d07bebe7d7d854d37
Nathakit Leemeetham <77628492+JackzazaTH@users.noreply.github.com>
2025-11-11 21:06:00 +0700

    Add files via upload

---
+++ bandd789/next-env.d.ts
@@ -0,0 +1,7 @@
+// FIX: Commenting out these references to resolve a "Cannot find type definition file" error,
+// which is likely caused by missing node_modules. This is a temporary workaround.
+// /// <reference types="next" />
+// /// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
